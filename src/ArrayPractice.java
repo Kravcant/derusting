83a0d639e31fa7479b2cac7a49995623723830fe
anthonykravchishin <tonykravc53@gmail.com>
2025-09-30 20:06:33 -0700

    ArrayPractice complete

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,18 +1,26 @@
+import java.util.*;
+
 public class ArrayPractice {
   public static void main(String[] args) {
     // Create an array of Strings of size 4
-
+    List<String> list = new ArrayList<>();
     // Set the value of the array at each index to be a different String
     // It's OK to do this one-by-one
-
+    list.add("First");
+    list.add("Second");
+    list.add("Third");
     // Get the value of the array at index 2
-
+    System.out.println(list.get(2));
     // Get the length of the array
-
+    System.out.println(list.size());
     // Iterate over the array using a traditional for loop and print out each item
-
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println(list.get(i));
+    }
     // Iterate over the array using a for-each loop and print out each item
-
+    for (String item : list) {
+        System.out.println(item);
+    }
     /*
      * Reminder!
      * 
